5e7d59103bbb7607d4ead770941735adc88abfdf
asimo <32599578+isossa@users.noreply.github.com>
2019-09-28 00:32:06 -0500

    Delete isSubstring method

--- The Algorithm Design Manual - Learning Track/Fundamental Algorithms/String Manipulation/patternMatching.java
+++ The Algorithm Design Manual - Learning Track/Fundamental Algorithms/String Manipulation/patternMatching.java
@@ -8,37 +8,6 @@
  */
 
 /**
- * Implements a pattern matching solver that identifies if a substring can be created out of another string by deleting characters.
- * 
- * @param str     Text string that may or may not contain the patter to match.
- * @param pattern Text pattern to build.
- * @return True if STR contains PATTERN.
- */
-public static boolean canConstructSubstring(String str, String pattern)
-{
-	// Initialize counters
-	int i = 0, j = 0;
-
-	// Get input strings lengths
-	final int STR_LENGTH = str.length();
-	final int PATTERN_LENGTH = pattern.length();
-
-	// Process each char in str => O(str.length)
-	for (i = 0; i < STR_LENGTH; i++)
-	{
-		if (pattern.charAt(j) == str.charAt(i))
-		{
-			if (j == PATTERN_LENGTH - 1)
-			{
-				return true;
-			}
-			j++;
-		}
-	}	
-	return false;
-}
-
-/**
  * Implements a pattern matching solver.
  * 
  * @param str     Text string that may or may not contain the patter to match.
