f5c85dc10b7987b03fa97d21417d6c40cfb66d97
Isidore Sossa <connect.with.isidore@gmail.com>
2020-04-10 23:53:59 -0500

    Modify WeirdAlgorithm.java so that input is read from stdin

--- CSES Problem Set - Learning Track/src/algorithms/introductoryProblems/WeirdAlgorithm.java
+++ CSES Problem Set - Learning Track/src/algorithms/introductoryProblems/WeirdAlgorithm.java
@@ -1,5 +1,9 @@
 package algorithms.introductoryProblems;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /*
  * PROBLEM STATEMENT
  * 
@@ -29,7 +33,11 @@ public class WeirdAlgorithm
 	 */
 	public static void main(String [] args)
 	{
-		solution(3);
+		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		
+		solution(in.nextInt());
+		
+		in.close();
 	}
 	
 	/**
